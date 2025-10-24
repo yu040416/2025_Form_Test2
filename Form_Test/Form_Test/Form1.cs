23cb00f053fb07237a7f4682aa032ee2ac08f1a2
hisui <yuukobi0403@gmail.com>
2025-10-24 15:33:36 +0900

    ボタン24個

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.PerformanceData;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,35 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+            int[,] matrix = new int[6,4];
+            int i, j;
+
+            for (i = 0; i < 6; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    //インスタンスの生成
+                    Test_Button testButton = new Test_Button();
+
+                    //ボタンの位置を設定
+                    testButton.Location = new Point(50 * i, 50 * j);
+
+                    //ボタンの大きさを設定
+                    testButton.Size = new Size(50, 50);
+
+                    //ボタン内テキストの設定
+                    testButton.Text = "あいうえお";
+
+                    //コントロールボタンを追加
+                    Controls.Add(testButton);
+                }
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C#の世界へようこそ!");
         }
     }
 }
